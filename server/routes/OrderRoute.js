474e8585b1c13d6a25dc28bead4ed511e5097eea
jeremiapakpahan <51002297+jeremiapakpahan@users.noreply.github.com>
2021-12-31 11:19:16 +0700

    Add files via upload

--- server/routes/OrderRoute.js
+++ server/routes/OrderRoute.js
@@ -6,14 +6,24 @@ import IndexCtrl from "../controllers/indexController";
 const router = Router()
 
 //router.post("/:id", IndexCtrl.CartController.create)
-router.get("/", IndexCtrl.Orders.findAll)
-router.get("/:id", IndexCtrl.Orders.findOne)
-router.post("/:id", IndexCtrl.Orders.create)
-router.put("/:id", IndexCtrl.Orders.update)
-router.delete("/:id",IndexCtrl.Orders.deleteRow)
-
-router.post("/", IndexCtrl.Orders.isCartExist, IndexCtrl.Orders.rekap, IndexCtrl.Orders.create)
+//router.post("/:id", IndexCtrl.Orders.create)
+router.post("/", IndexCtrl.CartController.summaryCart,IndexCtrl.Orders.updateStock)
 
 
+//router.post("/", IndexCtrl.Orders.isCartExist, IndexCtrl.Orders.rekap, IndexCtrl.Orders.create)
+
+// router.post("/createOrder", 
+// IndexCtrl.Orders.isCartExist, 
+// IndexCtrl.Orders.rekap, 
+// IndexCtrl.Orders.getOrderNumber, 
+// IndexCtrl.Orders.createOrder)
+
+//router
+router.post("/checkout",
+    IndexCtrl.CartController.summaryCart,
+    IndexCtrl.Orders.getOrderNumber,
+    IndexCtrl.Orders.newOrder,
+    IndexCtrl.Orders.updateStock,
+    IndexCtrl.Orders.updateStatus);
 
 export default router
